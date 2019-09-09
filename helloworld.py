ffbe3299e22204c866db8a6bef17a940ebc4b5ab
jschauer <jschauer@umich.edu>
2019-09-09 14:34:54 -0400

    Finished lab0

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+import random;
+print("Jay Schauer");
+n1 = random.randint(0,100);
+n2 = random.randint(0,100);
+print(n1);
+print(n2);
+print("Sum = " + str(n1 + n2));
+print("Average = " + str((n1 + n2) / 2.0));
